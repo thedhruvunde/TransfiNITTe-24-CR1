f4af58d2fa2303216e98cfb2d9ec36d5db193ef9
Dhruv Unde <dhruvunde@gmail.com>
2024-10-19 17:15:30 +0530

    Changes

--- Prompt/trial.py
+++ Prompt/trial.py
@@ -1,9 +1,19 @@
-from openai import api_key, OpenAI
-client = OpenAI(
-  api_key = "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"
+'''
+export TRANFINITTE_API_KEY="sk-proj-yo_CSrCd4B8lqMN98U7ld22028ZNeUU_QVW5sqdk4Yc9pRLaODtlbcnllbPdrRKZxsUb_7YdVQT3BlbkFJyJmt9JOzwsLHrdaulTBXu-GdEu-fgfxJ5J-ILZ_N
+2f_GhzlX24G8Zu-W9dgKSzygroAbU-tIoA"
+'''
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
 )
 
-completion = client.completions.create(model='gpt-3.5-turbo', prompt="Hi gpt")
-print(completion.choices[0].text)
-print(dict(completion).get('usage'))
-print(completion.model_dump_json(indent=2))
+print(completion.choices[0].message)
